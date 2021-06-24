9f0a42e4705d7278916982091deddf9e532401a2
C3bin <77668443+C3bin@users.noreply.github.com>
2021-06-24 11:54:21 +0200

    Add files via upload

---
+++ __resource.lua
@@ -0,0 +1,12 @@
+-- Made by C3bin
+
+resource_manifest_version '44febabe-d386-4d18-afbe-5e627f4af937'
+
+description 'Vytvorené pod autorskými právami. Vytvoril C3bin.'
+
+version '1.1'
+
+client_scripts {
+	'@es_extended/locale.lua',
+	'client/npc_drop-client.lua',
+}
